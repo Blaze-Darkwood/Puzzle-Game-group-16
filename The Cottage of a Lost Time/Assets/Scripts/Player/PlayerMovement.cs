6f0290454cccd0de06f3a917d53d97f29fa201f1
Gerben Bol <gerben.bol249@gmail.com>
2024-11-18 12:04:48 +0100

    player actions

--- The Cottage of a Lost Time/Assets/Scripts/Player/PlayerMovement.cs
+++ The Cottage of a Lost Time/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private Transform cam;
+
+    private Rigidbody rb;
+    private PlayerInputs input;
+    private Vector2 move = Vector2.zero;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        input = new();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Update()
+    {
+        // Movement
+        float speed = .5f;
+
+        if (rb.linearVelocity.magnitude + move.magnitude > maxSpeed)
+            speed = moveSpeed;
+
+        rb.AddRelativeForce(2 * speed * move);
+
+        // Ground check
+    }
+
+    private void OnMove()
+    {
+        // Store movement input
+        move = input.Player.Move.ReadValue<Vector2>();
+    }
 }
