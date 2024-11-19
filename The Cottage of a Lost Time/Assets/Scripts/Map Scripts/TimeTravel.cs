b867acca29a19b6b5318c6a13e6caeb7c222b944
Eduardo Lage <eduardo@camizao.com>
2024-11-19 15:14:24 +0100

    setting up test map and getting lovely crystal sprite

---
+++ The Cottage of a Lost Time/Assets/Scripts/Map Scripts/TimeTravel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTravel : MonoBehaviour
+{
+    [SerializeField] private Image crystalHud;
+    private GameObject futureMap;
+    private GameObject pastMap;
+
+    void Start()
+    {
+        //futureMap = make this find object with layer, make a 2nd one for the past map
+    }
+
+    void Update()
+    {
+        
+    }
+}
