da7fbe97e9e1120703ec615ed7e452df2ec92d86
Eduardo Lage <eduardo@camizao.com>
2024-12-03 15:19:06 +0100

    one random package and now we find the map with layers

--- The Cottage of a Lost Time/Assets/Scripts/Map Scripts/TimeTravel.cs
+++ The Cottage of a Lost Time/Assets/Scripts/Map Scripts/TimeTravel.cs
@@ -1,19 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Unity.Linq;
+using UnityEngine.UIElements;
 using UnityEngine.UI;
+
+enum MapTimes
+{
+    PastMap = 6,
+    FutureMap = 7
+}
 
 public class TimeTravel : MonoBehaviour
 {
-    [SerializeField] private Image crystalHud;
-    private GameObject futureMap;
-    private GameObject pastMap;
+    [SerializeField] private CanvasGroup crystalHud;
+    [SerializeField] private GameObject[] futureMap;
+    [SerializeField] private GameObject[] pastMap;
 
-    void Start()
+    private void Start()
     {
-        //futureMap = make this find object with layer, make a 2nd one for the past map
+        futureMap = FindGameObjectsInLayer((int)MapTimes.FutureMap);
+        pastMap = FindGameObjectsInLayer((int)MapTimes.PastMap);
     }
 
-    void Update()
+
+    GameObject[] FindGameObjectsInLayer(int layer)
     {
-        
+        var goArray = FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        var goList = new System.Collections.Generic.List<GameObject>();
+        for (int i = 0; i < goArray.Length; i++)
+        {
+            if (goArray[i].layer == layer)
+            {
+                goList.Add(goArray[i]);
+            }
+        }
+        if (goList.Count == 0)
+        {
+            return null;
+        }
+        return goList.ToArray();
     }
 }
